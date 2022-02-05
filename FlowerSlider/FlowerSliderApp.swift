dbdb3cebd40a5511d8a56684f48356eefaf697b0
Joey Graham <82058059+JKGRAHAMs@users.noreply.github.com>
2022-02-05 11:28:58 -0600

    Initial Commit

---
+++ FlowerSlider/FlowerSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowerSliderApp.swift
+//  FlowerSlider
+//
+//  Created by Joey Graham on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FlowerSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
